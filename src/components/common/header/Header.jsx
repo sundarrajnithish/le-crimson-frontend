b8378e7333d61cd81b26c6206dda2761b5c4a833
Sin1992 <ssindhiya1992@gmail.com>
2022-11-14 10:03:08 -0500

    Added Profile Dropdown

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,59 +1,66 @@
-import React, { useState } from "react"
-import Head from "./Head"
-import "./header.module.css"
-import { Link } from "react-router-dom"
+import React, { useState } from "react";
+import Head from "./Head";
+import "./header.module.css";
+import DropDownProfile from "./DropDownProfile";
+import { Link } from "react-router-dom";
 
-import Footer from "../footer/Footer"
+import Footer from "../footer/Footer";
 
 const Header = () => {
-  const [navbar, setNavbar] = useState(false)
+  const [navbar, setNavbar] = useState(false);
 
   return (
     <>
       <Head />
       <header>
-        <div className='container paddingSmall'>
+        <div className="container paddingSmall">
           <nav>
-            <ul className={navbar ? "navbar" : "flex"} onClick={() => setNavbar(false)}>
+            <ul
+              className={navbar ? "navbar" : "flex"}
+              onClick={() => setNavbar(false)}
+            >
               {/* <li>
               {navbar ? <i className='fa fa-times'></i> : <i className='fa fa-bars'></i>}
               <button className='barIcon' onClick={() => setNavbar(!navbar)} />
               </li> */}
-              
-              <li>
 
+              <li></li>
+              <li>
+                <Link to="/">Home</Link>
               </li>
               <li>
-                <Link to='/'>Home</Link>
+                <Link to="/sports">News</Link>
               </li>
               <li>
-                <Link to='/sports'>News</Link>
+                <Link to="/culture">Social</Link>
               </li>
               <li>
-                <Link to='/culture'>Social</Link>
+                <Link to="/preferences">Preferences</Link>
               </li>
               <li>
-                <Link to='/preferences'>Preferences</Link>
+                <Link to="/AboutUs">About Us</Link>
               </li>
               <li>
-                <Link to='/AboutUs'>About Us</Link>
-              </li>
-              <li>
-                <Link to='/memes'>Contact Us</Link>
+                <Link to="/memes">Contact Us</Link>
               </li>
               <ul>
-              <div className="searchbar">
-                <input type='text' placeholder="Search..." style = {{width:"800px", height:"25px"}} />
-              </div>
+                <div className="searchbar">
+                  <input
+                    type="text"
+                    placeholder="Search..."
+                    style={{ width: "800px", height: "25px" }}
+                  />
+                </div>
+              </ul>
+              <ul>
+                <DropDownProfile />
               </ul>
             </ul>
-             
-            
           </nav>
         </div>
       </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
