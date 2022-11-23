a919376d2445682613a103cf56cd80e1a54b0130
Sin1992 <ssindhiya1992@gmail.com>
2022-11-23 08:01:57 -0500

    updated chatbot

--- src/App.js
+++ src/App.js
@@ -1,37 +1,56 @@
-// CSS Files
-import './App.css';
+import { CometChat } from "@cometchat-pro/chat";
+import * as CONSTANTS from "./constants/constants";
+import { CometChatUI } from "./cometchat-pro-react-ui-kit/CometChatWorkspace/src/components";
 
-// Imports
-import React from 'react'
-import Loginpage from './components/login-page/Loginpage';
-import { Routes, Route } from "react-router-dom";
-import Homepages from './components/home-page/Homepages';
-import PreferencePage from './components/preferences/PreferencePage';
-import Contact from './components/contact/contactpage';
-import AboutUsPage from './components/about/AboutUsPage';
-import Profile from './components/profile/profilepage'
-import Search from './components/search/searchpage'
+import "./App.css";
 
-// import Cometchat from "./components/social/cometchat"
+const appid = CONSTANTS.APP_ID;
+const region = CONSTANTS.APP_REGION;
+const authKey = CONSTANTS.AUTH_KEY;
+const appSetting = new CometChat.AppSettingsBuilder()
+  .subscribePresenceForAllUsers()
+  .setRegion(region)
+  .build();
 
-// import Bootstrap from "./components/news-bootstrap/bootstrap"
+CometChat.init(appid, appSetting).then(
+  () => {
+    console.log("Initialization completed successfully");
+  },
+  (error) => {
+    console.log("Initialization failed with error:", error);
+    // Check the reason for error and take appropriate action.
+  }
+);
 
+//create dummy user
+
+const uid = "user2";
+//const name = "ss";
+//var user = new CometChat.User(uid);
+//user.setName(name);
+//CometChat.createUser(user, authKey).then(
+// (user) => {
+//  console.log("user created", user);
+// },
+//(error) => {
+//   console.log("error", error);
+// }
+//);
+
+CometChat.login(uid, authKey).then(
+  (user) => {
+    console.log("Login Successful:", { user });
+  },
+  (error) => {
+    console.log("Login failed with exception:", { error });
+  }
+);
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Loginpage />} />
-        <Route path="/home" element={<Homepages />} />
-        <Route path="/preferences" element={<PreferencePage />} />
-        <Route path="/contactus" element={<Contact />} />
-        <Route path="/aboutus" element={<AboutUsPage />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/search" element={<Search />} />
-        {/* <Route path="/boot" element={<Bootstrap />} /> */}
-        </Routes>
-  
-    </>
+    <div style={{ width: "800px", height: "800px" }}>
+      <CometChatUI />
+    </div>
   );
 }
 
