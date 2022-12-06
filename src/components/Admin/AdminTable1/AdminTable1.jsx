dbf4e1304b4e9d5725298785bc31d50d6182b499
Sin1992 <ssindhiya1992@gmail.com>
2022-12-06 11:57:07 -0500

    Modified Admin and user profile

--- src/components/Admin/AdminTable1/AdminTable1.jsx
+++ src/components/Admin/AdminTable1/AdminTable1.jsx
@@ -1,33 +1,43 @@
 import "./AdminTable1.css";
 import Table from "react-bootstrap/Table";
 
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Records from "./table.json";
 
 const AdminTable1 = () => {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    const getItems = async () => {
+      const response = await axios.get(
+        "https://lecrimson-backend.herokuapp.com/admin/profile/all"
+      );
+      console.log(response.data, "Data at AdminlistTable");
+      setItems(response.data);
+    };
+    getItems();
+  }, []);
+
   return (
     <div>
       <table sx={{ Width: 800 }} aria-label="simple table" className="table">
         <tr>
-          <th className="th">ID</th>
-          <th className="th">Name</th>
-          <th className="th">Email</th>
-          <th className="th">Location</th>
-          <th className="th">Source</th>
+          <th className="th2">Name</th>
 
-          <th className="th">Status</th>
+          <th className="th2">Email</th>
+          <th className="th2">Location</th>
+          <th className="th2">Source</th>
         </tr>
         <hr />
-        {Records &&
-          Records.map((record) => {
+        {items &&
+          items.map((record) => {
             return (
               <tr>
-                <td className="td">{record.id}</td>
-                <td className="td">{record.firstName}</td>
-                <td className="td">{record.email}</td>
-                <td className="td">{record.location}</td>
-                <td className="td">Google</td>
+                <td className="td2">{record.firstName}</td>
 
-                <td className="td">{record.status}</td>
+                <td className="td2">{record.email}</td>
+                <td className="td2">{record.location}</td>
+                <td className="td2">{record.loginSource}</td>
               </tr>
             );
           })}
--- src/components/user-profile/BlockTable/Blockeduser.jsx
+++ src/components/user-profile/BlockTable/Blockeduser.jsx
@@ -1,53 +1,62 @@
 import Records from "./data.json";
 import "./Blockeduser.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const BlockedUser = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const getItems = async () => {
+      const response = await axios.get(
+        "https://lecrimson-backend.herokuapp.com/profile?userId=14"
+      );
+      console.log(response.data, "Data at blockTable");
+      setItems(response.data);
+    };
+    getItems();
+  }, []);
+
+  const clickHandler = (event) => {
+    const newBlockedList = items.blocked.filter(
+      (block) => block.id !== parseInt(event.target.id)
+    );
+
+    setItems((prev) => ({
+      ...prev,
+      blocked: newBlockedList,
+    }));
+  };
+
   return (
-    <>
-    <div>
-      <h2>Blocked Users</h2>
-      <br />
-      {Records &&
-        Records.map((record) => {
-          return (
-            <div className="table-container" key={record.id}>
-              <table sx={{ minWidth: 650 }} aria-label="simple table">
-                <tr>
-                  <th>ID</th>
+    <div key={items.id}>
+      <table sx={{ minWidth: 650 }} aria-label="simple table">
+        <tr>
+          <th>Name</th>
+          <th>Status</th>
+          <th>.</th>
+        </tr>
 
-                  <th>Name</th>
-                  <th>Status</th>
-                  <th>.</th>
-                </tr>
-
-                {record.blocked &&
-                  record.blocked.map((data) => {
-                    return (
-                      <tr>
-                        <td>{data.id}</td>
-                        {/*<TableCell>
-                              <span className="user-logo">
-                                <img
-                                  src={require("./Profile-icon.png")}
-                                  alt="i"
-                                  className="pro-img"
-                                ></img>
-                              </span>
-                      </TableCell>*/}
-                        <td>{data.blockedProfileName}</td>
-                        <td>Blocked</td>
-                        <td>
-                          <button className="pro-button">UnBlock</button>
-                        </td>
-                      </tr>
-                    );
-                  })}
-              </table>
-            </div>
-          );
-        })}
+        {items.blocked &&
+          items.blocked.map((data) => {
+            return (
+              <tr>
+                <td>{data.BlockedProfileName}</td>
+                <td>Blocked</td>
+                <td>
+                  <button
+                    id={data.id}
+                    onClick={clickHandler}
+                    className="pro-button"
+                  >
+                    UnBlock
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+      </table>
     </div>
-    </>
   );
 };
 
