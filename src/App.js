aa80c34bd5dc610a4e1fc64c9613bd556fa6e359
Nitish Sundarraj <52373939+sundarrajnithish@users.noreply.github.com>
2022-12-05 19:19:35 -0500

    Changes in Chat

--- src/App.js
+++ src/App.js
@@ -15,6 +15,34 @@ import Social_page from "./components/pages/social_page/social_page";
 
 import "./components/chat/cometchat"
 
+
+// import { CometChat } from "@cometchat-pro/chat";
+import * as CONSTANTS from "./components/constants/constants";
+
+const authKey = CONSTANTS.AUTH_KEY;
+const uid = "user2"
+// const uid = localStorage.getItem("email")
+const name = localStorage.getItem("name")
+
+// var user = new CometChat.User(uid);
+// user.setName(name);
+// CometChat.createUser(user, authKey).then(
+//     user => {
+//         console.log("user created", user);
+//     },error => {
+//         console.log("error", error);
+//     }
+// )
+
+CometChat.login(uid, authKey).then(
+    (user) => {
+      console.log("Login Successful:", { user });
+    },
+    (error) => {
+      console.log("Login failed with exception:", { error });
+    }
+  );
+
 function App() {
   return (
     <>
